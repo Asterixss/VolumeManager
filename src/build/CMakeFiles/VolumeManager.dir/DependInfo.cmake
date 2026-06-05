4f75b715dbf5f436f033400d5f36676c9c960dca
yummyzzzz <alex.christian.sr@gmail.com>
2026-06-05 17:10:24 +0200

    Add files via upload

---
+++ src/build/CMakeFiles/VolumeManager.dir/DependInfo.cmake
@@ -0,0 +1,58 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "RC"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_RC
+  "C:/Users/spamt/Documents/VolumeMixer/app.rc" "C:/Users/spamt/Documents/VolumeMixer/build/Desktop_Qt_6_11_1_MinGW_64_bit-Release/CMakeFiles/VolumeManager.dir/app.rc.obj"
+  )
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_RC
+  "MINGW_HAS_SECURE_API=1"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NEEDS_QMAIN"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  "UNICODE"
+  "WIN32"
+  "WIN64"
+  "_ENABLE_EXTENDED_ALIGNED_STORAGE"
+  "_UNICODE"
+  "_WIN64"
+  )
+
+# The include file search paths:
+set(CMAKE_RC_TARGET_INCLUDE_PATH
+  "VolumeManager_autogen/include"
+  "C:/Qt/6.11.1/mingw_64/include/QtCore"
+  "C:/Qt/6.11.1/mingw_64/include"
+  "C:/Qt/6.11.1/mingw_64/mkspecs/win32-g++"
+  "C:/Qt/6.11.1/mingw_64/include/QtGui"
+  "C:/Qt/6.11.1/mingw_64/include/QtWidgets"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "VolumeManager_autogen/timestamp" "custom" "VolumeManager_autogen/deps"
+  "C:/Users/spamt/Documents/VolumeMixer/build/Desktop_Qt_6_11_1_MinGW_64_bit-Release/VolumeManager_autogen/mocs_compilation.cpp" "CMakeFiles/VolumeManager.dir/VolumeManager_autogen/mocs_compilation.cpp.obj" "gcc" "CMakeFiles/VolumeManager.dir/VolumeManager_autogen/mocs_compilation.cpp.obj.d"
+  "C:/Users/spamt/Documents/VolumeMixer/audiobackend.cpp" "CMakeFiles/VolumeManager.dir/audiobackend.cpp.obj" "gcc" "CMakeFiles/VolumeManager.dir/audiobackend.cpp.obj.d"
+  "C:/Users/spamt/Documents/VolumeMixer/main.cpp" "CMakeFiles/VolumeManager.dir/main.cpp.obj" "gcc" "CMakeFiles/VolumeManager.dir/main.cpp.obj.d"
+  "C:/Users/spamt/Documents/VolumeMixer/mainwindow.cpp" "CMakeFiles/VolumeManager.dir/mainwindow.cpp.obj" "gcc" "CMakeFiles/VolumeManager.dir/mainwindow.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/build/CMakeFiles/VolumeManager_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VolumeManager_autogen"
+  "VolumeManager_autogen/include/ui_mainwindow.h"
+  "VolumeManager_autogen/mocs_compilation.cpp"
+  "VolumeManager_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/VolumeManager_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
